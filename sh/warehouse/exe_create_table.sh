6deb9f176762f5cb8fd022221c6083bbbf936dea
hyunbar777 <hyunbar@126.com>
2020-08-09 19:38:40 +0800

    0809

---
+++ sh/warehouse/exe_create_table.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# 定义变量
+db=gmall
+home=/home/duoduo/bin/batch_create_table
+hive=/opt/module/hive-3.1.2/bin/hive
+tmp=/home/duoduo/bin
+if [ -f $tmp/tmp.tmp ]
+then
+    `rm -rf $tmp/tmp.tmp`
+fi
+`touch $tmp/tmp.tmp`
+
+function read(){
+for file in `ls $1`
+do
+    if [ -d $1"/"$file ]
+    then
+          read $1"/"$file
+    else
+
+          path=$1"/"$file
+          name=$file
+        echo "path=$path"
+        #echo "name=$name"
+        echo "use $db;" >> "$tmp/tmp.tmp"
+        echo  `cat $path` >> "$tmp/tmp.tmp"
+
+    fi
+done
+}
+read $home
+
+$hive -e "create database if not exists $db;use $db;"
+$hive -f "$tmp/tmp.tmp"
